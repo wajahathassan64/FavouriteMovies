0cf50ef6b5a54c686334458123448bb9c74fd2ac
Wajahat Hassan <wajahat@digitify.com>
2021-04-10 23:59:57 +0500

    Initial Commit

---
+++ AssignmentMovieDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssignmentMovieDemo
+//
+//  Created by Wajahat Hassan on 10/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
